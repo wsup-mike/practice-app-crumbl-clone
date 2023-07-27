e85d8868a7ac87b6a88658db3f0300aebdaf8513
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 09:34:11 -0500

    Wrap NavContainer around my app

--- App.js
+++ App.js
@@ -4,10 +4,12 @@ import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
