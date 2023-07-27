e423947ced8768c5f30d5a9de769dbe55d09a7b7
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 11:19:36 -0500

    Screen1

---
+++ screens/Screen1.jsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const screen1 = () => {
+  return (
+    <SafeAreaView>
+      <Text>Screen1</Text>
+    </SafeAreaView>
+  )
+}
+
+export default screen1
