c01de8da2ad1106c2a10328d9287ad1aa77d26bd
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 17:21:54 -0500

    Create Gesture2Screen

---
+++ screens/Gesture2Screen.jsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Gesture2Screen = () => {
+  return (
+    <SafeAreaView className='flex-1 justify-center'>
+      <Text className='text-3xl text-center font-extrabold'>Gesture2Screen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Gesture2Screen
