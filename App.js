71830371c7e7c665e6f2ea58cd33eed189273057
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 09:53:52 -0500

    import RootNavigator

--- App.js
+++ App.js
@@ -2,16 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import "react-native-gesture-handler";
+import RootNavigator from "./RootNavigator";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </NavigationContainer>
-  );
+  return <NavigationContainer></NavigationContainer>;
 }
 
 const styles = StyleSheet.create({
