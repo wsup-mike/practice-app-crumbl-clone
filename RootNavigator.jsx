7aa7042b329a28ba358b390e678b7ca8ff7a4507
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 09:55:21 -0500

    import status bar

--- RootNavigator.jsx
+++ RootNavigator.jsx
@@ -1,9 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar'
 
 const RootNavigator = () => {
   return (
-    <View style={styles.container}>
+    <View>
         <Text>The Root</Text>
         <StatusBar style="auto" />
       </View>
