89ca66f98aea020c73a06c5523980aa684c6ade8
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 11:11:16 -0500

    Cr8 Screen1

--- RootNavigator.jsx
+++ RootNavigator.jsx
@@ -1,13 +1,13 @@
-import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
 
 const Tab = createBottomTabNavigator();
 
 const RootNavigator = () => {
   return (
         <Tab.Navigator>
-
+      
         </Tab.Navigator>
     )
 }
