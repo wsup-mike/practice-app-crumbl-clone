b19d83af68925dac2e5a4264cd7dc8e01093c6b0
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 17:47:25 -0500

    Change bg color

--- screens/Gesture1Screen.jsx
+++ screens/Gesture1Screen.jsx
@@ -4,14 +4,10 @@ import { Button } from 'react-native';
 
 const Gesture1Screen = () => {
   return (
-    // <View style={{flex: 1,  alignItems: 'center', justifyContent: 'center'}}>
-  <SafeAreaView className='flex-1'>
-    <View className='bg-pink-400 space-y-4'>
-      <Text className='text-3xl font-extralight'>Hello world</Text>
-      <View className='h-5 w-auto bg-slate-500' />
-      <View className='h-5 w-auto bg-slate-500' />
-      <View className='h-5 w-auto bg-slate-500' />
-      <View className='h-5 w-auto bg-slate-500' />
+  <SafeAreaView className='flex-1 bg-lime-400 justify-center'>
+    <View className=''>
+      <Text className='text-3xl font-extralight text-white text-center'>Gesture1Screen</Text>
+      
       
     </View>
   </SafeAreaView>
