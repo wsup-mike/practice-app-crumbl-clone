72f2d00788350d88a5a7c9a5ce57619677e01dd6
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 11:19:46 -0500

    imoprt stack1

--- RootNavigator.jsx
+++ RootNavigator.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-
+import stack1 from './stacks/stack1';
 
 const Tab = createBottomTabNavigator();
 
 const RootNavigator = () => {
   return (
         <Tab.Navigator>
-      
+            <Tab.Screen name='stack1' component={stack1}/>
         </Tab.Navigator>
     )
 }
