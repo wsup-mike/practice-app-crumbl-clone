62f3694402544367befd5249c527861e5be748dd
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 17:18:49 -0500

    Add new color to view

--- screens/Gesture1Screen.jsx
+++ screens/Gesture1Screen.jsx
@@ -6,14 +6,13 @@ const Gesture1Screen = () => {
   return (
     // <View style={{flex: 1,  alignItems: 'center', justifyContent: 'center'}}>
   <SafeAreaView className='flex-1'>
-    <View className='bg-pink-400'>
+    <View className='bg-pink-400 space-y-4'>
       <Text className='text-3xl font-extralight'>Hello world</Text>
-      <Button title='This is a button'/>
-      <Button title='This is a button'/>
-      <Button title='This is a button'/>
-      <Button title='This is a button'/>
-      <Button title='This is a button'/>
-      <Button title='This is a button'/>
+      <View className='h-5 w-auto bg-slate-500' />
+      <View className='h-5 w-auto bg-slate-500' />
+      <View className='h-5 w-auto bg-slate-500' />
+      <View className='h-5 w-auto bg-slate-500' />
+      
     </View>
   </SafeAreaView>
   )
