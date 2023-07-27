b9c91b351d9bb76e0dab59eb4fc20bf45c20417a
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 13:14:29 -0500

    test classname

--- screens/Gesture1Screen.jsx
+++ screens/Gesture1Screen.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 const Gesture1Screen = () => {
   return (
-    <View style={{flex: 1,  alignItems: 'center', justifyContent: 'center'}}>
+    // <View style={{flex: 1,  alignItems: 'center', justifyContent: 'center'}}>
+    <View classname='bg-slate-500'>
       <Text style={{ fontSize: 30 }}>Gesture1Screen</Text>
     </View>
   )
