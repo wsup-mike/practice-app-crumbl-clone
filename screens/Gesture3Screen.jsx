4f9365ae586c800f53078c2245092198fc682380
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 17:33:47 -0500

    Gesture3Scren

---
+++ screens/Gesture3Screen.jsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Gesture3Screen = () => {
+  return (
+    <SafeAreaView className='justify-center flex-1'>
+      <Text className='text-center text-3xl font-semibold'>Gesture3Screen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Gesture3Screen
