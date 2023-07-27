2d5d58cdcfcdade070800c9d7202f32f34196570
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 17:33:33 -0500

    Cr8 Gesture4Screen

---
+++ screens/Gesture4Screen.jsx
@@ -0,0 +1,13 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Gesture4Screen = () => {
+  return (
+    <SafeAreaView className='flex-1 justify-center'>
+      <Text className='text-3xl font-semibold text-center'>Gesture4Screen</Text>
+    </SafeAreaView>
+  )
+
+}
+
+export default Gesture4Screen
