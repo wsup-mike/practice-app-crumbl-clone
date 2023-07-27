8c47f5c0fd858813d6313be30896fc0a1db7386c
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 11:57:26 -0500

    Create Gesture1Screen

---
+++ Gesture1Screen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Gesture1Screen = () => {
+  return (
+    <View style={{flex: 1,  alignContent: 'center', justifyContent: 'center'}}>
+      <Text>Gesture1Screen</Text>
+    </View>
+  )
+}
+
+export default Gesture1Screen
