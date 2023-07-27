53280d98ea9ad8358c678bfb39786fbcda1fdfd9
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 09:45:33 -0500

    imprt rn gesture handler

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
 
 export default function App() {
   return (
