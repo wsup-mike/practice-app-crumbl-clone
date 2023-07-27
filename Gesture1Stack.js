fbe34098c1f55c1d9758bf51a58d81320fb3905e
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 11:59:44 -0500

    imoprt Gesture1Scren

--- Gesture1Stack.js
+++ Gesture1Stack.js
@@ -1,13 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import Gesture1Screen from "./Gesture1Screen";
 
 const Stack = createStackNavigator();
 
 const Gesture1Stack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Gesture1" component={Gesture2Screen} />
+      <Stack.Screen name="Gesture1" component={Gesture1Screen} />
     </Stack.Navigator>
   );
 };
