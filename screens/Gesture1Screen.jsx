47726a7444212c01506a0e2f11e968834d24aa5e
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 17:15:01 -0500

    test new color

--- screens/Gesture1Screen.jsx
+++ screens/Gesture1Screen.jsx
@@ -1,21 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import { Button } from 'react-native';
 
 const Gesture1Screen = () => {
   return (
     // <View style={{flex: 1,  alignItems: 'center', justifyContent: 'center'}}>
-  <View className='flex-1'>
+  <SafeAreaView className='flex-1'>
     <View className='bg-pink-400'>
-    <Text className='text-3xl'>Hello world</Text>
-    <Button title='This is a button'/>
-    <Button title='This is a button'/>
-    <Button title='This is a button'/>
-    <Button title='This is a button'/>
-    <Button title='This is a button'/>
-    <Button title='This is a button'/>
+      <Text className='text-3xl font-extralight'>Hello world</Text>
+      <Button title='This is a button'/>
+      <Button title='This is a button'/>
+      <Button title='This is a button'/>
+      <Button title='This is a button'/>
+      <Button title='This is a button'/>
+      <Button title='This is a button'/>
     </View>
-  </View>
+  </SafeAreaView>
   )
 }
 
