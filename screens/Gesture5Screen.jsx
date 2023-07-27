2881df5a6e9878b7564b59e219e28fafc27a311f
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 17:33:18 -0500

    Cr8 Gesture5Screen

---
+++ screens/Gesture5Screen.jsx
@@ -0,0 +1,12 @@
+import { View, Text ,SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Gesture5Screen = () => {
+  return (
+    <SafeAreaView className='flex-1 justify-center'>
+      <Text className='text-center font-semibold text-3xl'>Gesture5Screen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Gesture5Screen
