b66fbd2dc98f0cb47a2b556bcc987b90f393ce1f
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 14:50:13 -0500

    Next commit

--- screens/Gesture1Screen.jsx
+++ screens/Gesture1Screen.jsx
@@ -1,12 +1,21 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Button } from 'react-native';
 
 const Gesture1Screen = () => {
   return (
     // <View style={{flex: 1,  alignItems: 'center', justifyContent: 'center'}}>
-    <View classname='flex-1 bg-slate-500 justify-center'>
-      <Text classname='text-3xl text-center'>Gesture1Screen</Text>
+  <View className='flex-1'>
+    <View className='bg-pink-400'>
+    <Text className='text-3xl'>Hello world</Text>
+    <Button title='This is a button'/>
+    <Button title='This is a button'/>
+    <Button title='This is a button'/>
+    <Button title='This is a button'/>
+    <Button title='This is a button'/>
+    <Button title='This is a button'/>
     </View>
+  </View>
   )
 }
 
