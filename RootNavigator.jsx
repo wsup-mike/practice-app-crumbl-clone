c7c00de228ea3532d14d479fc18325e0162578ee
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-27 10:19:07 -0500

    import createBottomTabNav

--- RootNavigator.jsx
+++ RootNavigator.jsx
@@ -1,6 +1,6 @@
-
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 const RootNavigator = () => {
   return (
