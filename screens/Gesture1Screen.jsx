3464d2c7d822653f1fbd6fd5ece70254432e5b58
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-29 08:31:18 -0500

    imoprt Animated

--- screens/Gesture1Screen.jsx
+++ screens/Gesture1Screen.jsx
@@ -1,12 +1,12 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Animated } from 'react-native'
 import React from 'react'
 import { Button } from 'react-native';
 
 const Gesture1Screen = () => {
   return (
-  <SafeAreaView className='flex-1 bg-lime-400 justify-center'>
+  <SafeAreaView className='flex-1 bg-lime-400'>
     <View className=''>
-      <Text className='text-3xl font-extralight text-white text-center'>Gesture1Screen</Text>
+      <Text className='text-3xl font-extralight text-white text-center mt-8'>Gesture1Screen</Text>
       
       
     </View>
