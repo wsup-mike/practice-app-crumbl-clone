fc80f9edb26633df4c96a291c422844c307c8f1e
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-29 10:39:08 -0500

    Add square

--- screens/Gesture3Screen.jsx
+++ screens/Gesture3Screen.jsx
@@ -2,18 +2,33 @@ import { View, Text, SafeAreaView, Pressable, Animated } from 'react-native'
 import React from 'react'
 
 const Gesture3Screen = () => {
+
+  const animation = () => {
+
+  };
+
   return (
     <SafeAreaView className='flex-1 bg-green-300'>
       <Text className='text-center text-3xl font-extralight text-white mt-3'>Gesture3Screen</Text>
       <Text className='text-white text-xl text-center'>Interpolation Practice</Text>
 
       <Pressable 
-        onPress={animation}
+        onPress={() => {}}
       >
         <View className='h-10 w-60 bg-orange-600 p-1 ml-3 mt-4 justify-center rounded-md'>
-          <Text className='text-center text-white'>Press to Animate - In Sequence</Text>
+          <Text className='text-center text-white'>Press to see 3 animations in 1</Text>
         </View>
       </Pressable>
+
+      <Animated.View 
+        className='h-16 w-16 bg-yellow-500 mt-3 ml-3'
+        style={{
+          transform: [
+      
+          ]
+        }}
+      />
+
     </SafeAreaView>
   )
 }
