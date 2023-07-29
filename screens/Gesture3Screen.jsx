b2b922e8253467bacc4f2a149bcebbe1547aff54
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-29 10:44:11 -0500

    Add duration 1000

--- screens/Gesture3Screen.jsx
+++ screens/Gesture3Screen.jsx
@@ -6,7 +6,11 @@ const Gesture3Screen = () => {
   const translation = useRef(new Animated.Value(0)).current
 
   const animation = () => {
-
+    Animated.timing(translation, {
+      toValue: 350,
+      useNativeDriver: true,
+      duration: 1000,
+    })
   };
 
   return (
@@ -15,7 +19,7 @@ const Gesture3Screen = () => {
       <Text className='text-white text-xl text-center'>Interpolation Practice</Text>
 
       <Pressable 
-        onPress={() => {}}
+        onPress={animation}
       >
         <View className='h-10 w-60 bg-orange-600 p-1 ml-3 mt-4 justify-center rounded-md'>
           <Text className='text-center text-white'>Press to see 3 animations in 1</Text>
@@ -26,7 +30,7 @@ const Gesture3Screen = () => {
         className='h-16 w-16 bg-yellow-500 mt-3 ml-3'
         style={{
           transform: [
-      
+            { translateX: translation }
           ]
         }}
       />
