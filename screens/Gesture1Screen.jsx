e6dc7c1f6808970b359cef61acf81e9235d6febf
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-29 10:32:30 -0500

    Add 1st animation

--- screens/Gesture1Screen.jsx
+++ screens/Gesture1Screen.jsx
@@ -1,25 +1,45 @@
-import { View, Text, SafeAreaView, Animated } from 'react-native'
+import { View, Text, SafeAreaView, Animated, Pressable, Button } from 'react-native'
 import React, { useEffect, useRef } from 'react'
 
 const Gesture1Screen = () => {
 
   const translation = useRef(new Animated.Value(0)).current
+  const translation2 = useRef(new Animated.Value(0)).current
 
   useEffect(() => {
     Animated.timing(translation, {
-      toValue: 50,
+      toValue: 300,
       useNativeDriver: true,
     }).start()
   }, []);
 
   return (
   <SafeAreaView className='flex-1 bg-lime-400'>
-    <View className=''>
+    <View>
       <Text className='text-3xl font-extralight text-white text-center mt-8'>Gesture1Screen</Text>
     </View>
-    <Text className='text-center text-xl font-extralight text-white mt-3 ml-3'>Left to Right: 50px distance - Animated.timing( )</Text>
+
+    <Text className='text-center text-xl font-extralight text-white mt-3 ml-3'>Left to Right: 300px distance - Animated.timing( )</Text>
     <Animated.View 
       className='w-14 h-14 bg-teal-500 mt-3 ml-3'
+      style={{
+        transform: [{translateX: translation}],
+      }}
+    />
+
+    <Text className='text-center text-xl font-extralight text-white mt-3 ml-3'>Or Press to activate animation: L - R 300</Text>
+    {/* <View className='w-32 h-12 bg-red-400 rounded-lg justify-center self-center'><Text className='text-center text-white'>Tap to Animate</Text></View> */}
+    <Button title='Tap to Animate' onPress={() => {
+      Animated.timing(translation2, {
+        toValue: 300,
+        useNativeDriver: true,
+      }).start()
+    }} />
+    <Animated.View 
+      className='w-14 h-14 bg-teal-500 mt-3 ml-3'
+      style={{
+        transform: [{translateX: translation2}],
+      }}
     />
   </SafeAreaView>
   )
