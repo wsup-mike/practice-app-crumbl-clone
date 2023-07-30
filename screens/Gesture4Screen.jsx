3dc45c673a191dee578381bb9a2527c63ae195d0
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-30 12:00:58 -0500

    Remove position absolute

--- screens/Gesture4Screen.jsx
+++ screens/Gesture4Screen.jsx
@@ -12,8 +12,21 @@ const Gesture4Screen = (props) => {
   const dimensions = useWindowDimensions();
 
   return (
-    <SafeAreaView className='flex-1 justify-center bg-stone-400'>
-      <Text className='text-3xl font-extralight text-white text-center'>Gesture4Screen</Text>
+    <SafeAreaView className='flex-1 bg-stone-400'>
+      <Text className='text-3xl font-extralight text-white text-center mt-3'>Gesture4Screen</Text>
+      <View
+        style={{
+          position: 'absolute',
+          left: dimensions.width / 2 - CURSOR_HALF_SIDE_SIZE,
+          top: dimensions.height / 2 - CURSOR_HALF_SIDE_SIZE, 
+          height: CURSOR_SIDE_SIZE,
+          width: CURSOR_SIDE_SIZE,
+          borderRadius: CURSOR_HALF_SIDE_SIZE,
+          backgroundColor: 'orange',
+        }}
+      >
+        
+      </View>
     </SafeAreaView>
   )
 
