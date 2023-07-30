d75b45c83f522335db005cdef996b91eaaf88e81
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-30 11:50:36 -0500

    import Animated

--- screens/Gesture4Screen.jsx
+++ screens/Gesture4Screen.jsx
@@ -1,11 +1,13 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Animated } from 'react-native'
 import React, { useRef } from 'react'
 
 const CURSOR_SIDE_SIZE = 20;
 const CURSOR_HALF_SIDE_SIZE = CURSOR_SIDE_SIZE / 2;
 
 const Gesture4Screen = (props) => {
+  const touch = useRef(
   
+  ).current;
 
   return (
     <SafeAreaView className='flex-1 justify-center bg-stone-400'>
