7ef19facae7ed2dab76f2552ed8cc9f2c1a2393b
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-30 11:49:31 -0500

    Set up 2 constants

--- screens/Gesture4Screen.jsx
+++ screens/Gesture4Screen.jsx
@@ -1,7 +1,12 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 
-const Gesture4Screen = () => {
+const CURSOR_SIDE_SIZE = 20;
+const CURSOR_HALF_SIDE_SIZE = CURSOR_SIDE_SIZE / 2;
+
+const Gesture4Screen = (props) => {
+  
+
   return (
     <SafeAreaView className='flex-1 justify-center bg-stone-400'>
       <Text className='text-3xl font-extralight text-white text-center'>Gesture4Screen</Text>
