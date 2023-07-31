39ef1e28b7de374efe823dfb401bf2d4650e3b7a
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-31 12:22:02 -0500

    Add <Image>

--- screens/Gesture5Screen.jsx
+++ screens/Gesture5Screen.jsx
@@ -1,10 +1,16 @@
-import { View, Text ,SafeAreaView } from 'react-native'
+import { View, Text ,SafeAreaView, Image } from 'react-native'
 import React from 'react'
+
 
 const Gesture5Screen = () => {
   return (
-    <SafeAreaView className='flex-1 justify-center bg-indigo-500'>
-      <Text className='text-center font-extralight text-white text-3xl'>Gesture5Screen</Text>
+    <SafeAreaView className='flex-1 items-center justify-center bg-indigo-500'>
+      <Text className='text-center font-extralight text-white text-3xl'>The Kiddos</Text>
+      <Image 
+        style={{width: 350, height: 500}}
+        className='rounded-xl'
+        source={require('../assets/Cousins.jpg')}
+      />
     </SafeAreaView>
   )
 }
