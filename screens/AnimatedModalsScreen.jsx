b666a287b467e3697367e0ec898ce192944fe861
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-01 08:09:28 -0500

    Create onClose and handleCloseMOdal hanldrer

--- screens/AnimatedModalsScreen.jsx
+++ screens/AnimatedModalsScreen.jsx
@@ -4,11 +4,16 @@ import { Button } from 'react-native';
 import Modal1 from './Modal1';
 
 const AnimatedModalsScreen = () => {
+
+  handleCloseModal = () => {
+    
+  }
+
   return (
     <SafeAreaView className='flex-1 items-center bg-indigo-400'>
       <Text className='text-center font-extralight text-3xl mt-3 text-white'>AnimatedModalsScreen</Text>
       <Button title='Show Modal1' onPress={() => {}}/>
-      <Modal1 />
+      <Modal1 onClose={handleCloseModal} />
     </SafeAreaView>
   )
 }
