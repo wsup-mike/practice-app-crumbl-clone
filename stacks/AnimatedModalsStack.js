80926e9df44410f9cf8498d3eac25c24b179821d
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-01 07:35:01 -0500

    Cr8 AnimatedModalsStack stack file

---
+++ stacks/AnimatedModalsStack.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const AnimatedModalsStack = () => {
+  return (
+    <View>
+      <Text>AnimatedModalsStack</Text>
+    </View>
+  );
+};
+
+export default AnimatedModalsStack;
