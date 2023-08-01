d730f379f88fb8c76a9f5cdc7bb6836e4fae68a4
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-01 08:03:03 -0500

    Add Button

--- screens/Modal1.jsx
+++ screens/Modal1.jsx
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Button } from 'react-native';
 
 const Modal1 = () => {
   return (
-    <View className='bg-white h-2/3 w-10/12'>
-      <Text>Modal1</Text>
+    <View className=' items-center w-full h-full'>
+      <View className='bg-white h-2/3 w-10/12 items-center justify-center rounded-xl'>
+        <Button title='Close Modal' onPress={() =>{}}/>
+      </View>
     </View>
   )
 }
