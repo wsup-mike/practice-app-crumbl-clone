c72ece2a20fd83b9f4b17ad194bc77bbcbb96f96
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-01 08:12:06 -0500

    Cr8 handleShowModal + set 2 onPress

--- screens/AnimatedModalsScreen.jsx
+++ screens/AnimatedModalsScreen.jsx
@@ -6,13 +6,17 @@ import Modal1 from './Modal1';
 const AnimatedModalsScreen = () => {
 
   handleCloseModal = () => {
-    
+
+  }
+
+  handleShowModal = () => {
+
   }
 
   return (
     <SafeAreaView className='flex-1 items-center bg-indigo-400'>
       <Text className='text-center font-extralight text-3xl mt-3 text-white'>AnimatedModalsScreen</Text>
-      <Button title='Show Modal1' onPress={() => {}}/>
+      <Button title='Show Modal1' onPress={handleShowModal}/>
       <Modal1 onClose={handleCloseModal} />
     </SafeAreaView>
   )
