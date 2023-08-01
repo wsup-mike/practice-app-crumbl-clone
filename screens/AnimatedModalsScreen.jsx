635e4524377974546a9b0ec1b585b0da1fbe1948
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-01 08:16:01 -0500

    in Modal1 visible={visible}

--- screens/AnimatedModalsScreen.jsx
+++ screens/AnimatedModalsScreen.jsx
@@ -18,7 +18,7 @@ const AnimatedModalsScreen = () => {
     <SafeAreaView className='flex-1 items-center bg-indigo-400'>
       <Text className='text-center font-extralight text-3xl mt-3 text-white'>AnimatedModalsScreen</Text>
       <Button title='Show Modal1' onPress={handleShowModal}/>
-      <Modal1 onClose={handleCloseModal} />
+      <Modal1 visible={visible} onClose={handleCloseModal} />
     </SafeAreaView>
   )
 }
