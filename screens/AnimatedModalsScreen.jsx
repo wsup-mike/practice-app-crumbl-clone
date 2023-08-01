d55a43ff2bbe49cc6981951896c9be43d5c7cc37
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-01 18:59:19 -0500

    Comment out suggestions

--- screens/AnimatedModalsScreen.jsx
+++ screens/AnimatedModalsScreen.jsx
@@ -5,8 +5,8 @@ import Modal1 from './Modal1';
 
 const AnimatedModalsScreen = () => {
   const [visible, setVisible] = useState(false)
-  const { height } = Dimensions.get('window'); // this
-    const transY = useRef(new Animated.Value(0)); // this
+  // const { height } = Dimensions.get('window'); // this
+  //   const transY = useRef(new Animated.Value(0)); // this
 
   handleShowModal = () => {
     setVisible(true)
@@ -17,11 +17,11 @@ const AnimatedModalsScreen = () => {
     setVisible(false)
   }
 
-  const scaleValue = transY.current.interpolate({ //this
-    inputRange: [0, height],
-    outputRange: [1, 0.9], // Apply a scaling effect from 100% to 90%
-    extrapolate: 'clamp',
-  });
+  // const scaleValue = transY.current.interpolate({ //this
+  //   inputRange: [0, height],
+  //   outputRange: [1, 0.9], // Apply a scaling effect from 100% to 90%
+  //   extrapolate: 'clamp',
+  // });
 
  
   return (
@@ -29,9 +29,9 @@ const AnimatedModalsScreen = () => {
       <View className='absolute w-full h-full bg-black' />
       <Animated.SafeAreaView 
         className='flex-1 items-center bg-indigo-400'
-        style={{ 
-          transform: [{ scale: scaleValue }]
-        }}
+        // style={{ 
+        //   transform: [{ scale: scaleValue }]
+        // }}
       >
         <Text className='text-center font-extralight text-3xl mt-3 text-white'>AnimatedModalsScreen</Text>
         <Button title='Show Modal1' onPress={handleShowModal}/>
