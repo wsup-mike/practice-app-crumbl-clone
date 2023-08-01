9db293ee4b48b85f8dc77b5c1617caa035e0ea91
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-01 08:38:04 -0500

    Set transform translateY transY.current

--- screens/Modal1.jsx
+++ screens/Modal1.jsx
@@ -2,8 +2,8 @@ import { View, Text, Button, Dimensions, Animated } from 'react-native'
 import React, { useEffect, useRef } from 'react'
 
 const Modal1 = ({ onClose, visible }) => {
+    
     const { height } = Dimensions.get('screen');
-
     const transY = useRef(new Animated.Value(height))
 
 
@@ -15,7 +15,7 @@ const Modal1 = ({ onClose, visible }) => {
         <View 
             className='absolute items-center w-full h-full'
             style={{ 
-                transform: [{ translateY: height }]
+                transform: [{ translateY: transY.current }]
             }}
         >
             <View className='bg-white h-2/3 w-10/12 items-center justify-center rounded-xl'>
