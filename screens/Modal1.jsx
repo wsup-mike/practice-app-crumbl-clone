6df36389e9113d0344bc27993bad0b7e0731cfb0
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-01 08:44:54 -0500

    Add our useEffect to animate modla

--- screens/Modal1.jsx
+++ screens/Modal1.jsx
@@ -1,11 +1,22 @@
 import { View, Text, Button, Dimensions, Animated } from 'react-native'
 import React, { useEffect, useRef } from 'react'
+import { Easing } from 'react-native';
 
 const Modal1 = ({ onClose, visible }) => {
     
     const { height } = Dimensions.get('screen');
     const transY = useRef(new Animated.Value(height))
 
+    useEffect(() => {
+        if (visible) {
+            Animated.timing(transY.current, {
+                toValue: 0,
+                duration: 2000,
+                easing: Easing.inOut(Easing.ease),
+                useNativeDriver: true,
+            }).start()
+        }
+    }, [visible]);
 
     const onPress = () => {
         onClose();
