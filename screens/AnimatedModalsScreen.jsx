c866800c29a101ce7e5fa7fc2469b1e582efaa66
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-01 08:03:21 -0500

    import Modal1 component

--- screens/AnimatedModalsScreen.jsx
+++ screens/AnimatedModalsScreen.jsx
@@ -1,12 +1,14 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import { Button } from 'react-native';
+import Modal1 from './Modal1';
 
 const AnimatedModalsScreen = () => {
   return (
     <SafeAreaView className='flex-1 items-center bg-indigo-400'>
       <Text className='text-center font-extralight text-3xl mt-3 text-white'>AnimatedModalsScreen</Text>
       <Button title='Show Modal1' onPress={() => {}}/>
+      <Modal1 />
     </SafeAreaView>
   )
 }
