44aed6b7e4a2449b177385787d39cec45ebd3b34
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-01 18:47:03 -0500

    Add scaleValue interoplation

--- screens/AnimatedModalsScreen.jsx
+++ screens/AnimatedModalsScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Dimensions } from 'react-native'
 import React, { useState } from 'react'
 import { Button } from 'react-native';
 import Modal1 from './Modal1';
@@ -17,11 +17,22 @@ const AnimatedModalsScreen = () => {
     setVisible(false)
   }
 
+  const scaleValue = transY.current.interpolate({ //this
+    inputRange: [0, height],
+    outputRange: [1, 0.9], // Apply a scaling effect from 100% to 90%
+    extrapolate: 'clamp',
+  });
+
  
   return (
     <>
       <View className='absolute w-full h-full bg-black' />
-      <SafeAreaView className='flex-1 items-center bg-indigo-400 '>
+      <SafeAreaView 
+        className='flex-1 items-center bg-indigo-400'
+        style={{ 
+          transform: [{ scale: scaleValue }]
+        }}
+      >
         <Text className='text-center font-extralight text-3xl mt-3 text-white'>AnimatedModalsScreen</Text>
         <Button title='Show Modal1' onPress={handleShowModal}/>
         <Modal1 visible={visible} onClose={handleCloseModal} />
