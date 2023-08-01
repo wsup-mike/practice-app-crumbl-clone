90025265ce17ce9471ea7e5961dd7148e6cd03fd
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-01 08:08:58 -0500

    Cr8 onPress and pass in props onClose

--- screens/Modal1.jsx
+++ screens/Modal1.jsx
@@ -2,14 +2,19 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Button } from 'react-native';
 
-const Modal1 = () => {
-  return (
-    <View className=' items-center w-full h-full'>
-      <View className='bg-white h-2/3 w-10/12 items-center justify-center rounded-xl'>
-        <Button title='Close Modal' onPress={() =>{}}/>
-      </View>
-    </View>
-  )
+const Modal1 = ({ onClose }) => {
+
+    const onPress = () => {
+        onClose();
+    }
+
+    return (
+        <View className=' items-center w-full h-full'>
+            <View className='bg-white h-2/3 w-10/12 items-center justify-center rounded-xl'>
+                <Button title='Close Modal' onPress={onPress}/>
+            </View>
+        </View>
+    )
 }
 
 export default Modal1
