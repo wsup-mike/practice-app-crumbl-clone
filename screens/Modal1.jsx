156775c8e080d1f1c80155ce52238cf0192de7d0
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-01 07:51:09 -0500

    Cr8 Modal1 modal

---
+++ screens/Modal1.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Modal1 = () => {
+  return (
+    <View className='bg-white h-2/3 w-10/12'>
+      <Text>Modal1</Text>
+    </View>
+  )
+}
+
+export default Modal1
