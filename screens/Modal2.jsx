6aa547cfa575f0e8f9152ba2bd1ed676e94bea5f
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-02 08:25:42 -0500

    import pacakges

---
+++ screens/Modal2.jsx
@@ -0,0 +1,13 @@
+import { View, Text, Button, Dimensions, Animated } from 'react-native'
+import React, { useEffect, useRef } from 'react'
+import { Easing } from 'react-native';
+
+const Modal2 = () => {
+  return (
+    <View>
+      <Text>Modal2</Text>
+    </View>
+  )
+}
+
+export default Modal2
