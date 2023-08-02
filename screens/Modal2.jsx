83c6cb6952faf54190c4aa4bba053d8f2bc0c806
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-02 08:26:19 -0500

    Pass in props onClose, visible

--- screens/Modal2.jsx
+++ screens/Modal2.jsx
@@ -2,7 +2,7 @@ import { View, Text, Button, Dimensions, Animated } from 'react-native'
 import React, { useEffect, useRef } from 'react'
 import { Easing } from 'react-native';
 
-const Modal2 = () => {
+const Modal2 = ({ onClose, visible }) => {
   return (
     <View>
       <Text>Modal2</Text>
