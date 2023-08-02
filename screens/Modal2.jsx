e6da5fdeff31a14d456202c2cbfd5654e513f29e
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-02 08:53:22 -0500

    Cr8 useEffect hook

--- screens/Modal2.jsx
+++ screens/Modal2.jsx
@@ -2,11 +2,37 @@ import { View, Text, Button, Dimensions, Animated } from 'react-native'
 import React, { useEffect, useRef } from 'react'
 import { Easing } from 'react-native';
 
-const Modal2 = ({ onClose, visible }) => {
-  return (
-    <View>
-      <Text>Modal2</Text>
-    </View>
+const Modal2 = ({ onClose, visible2 }) => { // Pass in from parent: handler n state 'visible2' 2 be used here
+
+    const { height, width } = Dimensions.get('screen') // get dimensions of user's existing screen height and width
+    
+    const transX = useRef(new Animated.Value(width)) // 2 hold a new ref value named transX to animate on
+
+    // A useEffect runs on both: initial render + whenever 'visible' changes
+    useEffect(() => {
+        if (visible2) {
+            Animated.timing(transX.current, {
+                toValue: 0,
+                duration: 225,
+                easing: Easing.inOut(Easing.ease),
+                useNativeDriver: true,
+            }).start()
+        } else {
+            Animated.timing(transX.current, {
+                toValue: width,
+                duration: 225,
+                easing: Easing.inOut(Easing.ease),
+                useNativeDriver: true,
+            }).start()
+        }
+    }, [visible2])
+
+    
+    
+    return (
+        <View>
+            <Text>Modal2</Text>
+        </View>
   )
 }
 
