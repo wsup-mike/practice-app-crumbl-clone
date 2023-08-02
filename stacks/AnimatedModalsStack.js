8d44902cb9127145074d4d6d53971c19a97e084c
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-02 09:30:50 -0500

    import Modal1 and 2

--- stacks/AnimatedModalsStack.js
+++ stacks/AnimatedModalsStack.js
@@ -2,6 +2,8 @@ import { View, Text } from "react-native";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import AnimatedModalsScreen from "../screens/AnimatedModalsScreen";
+import Modal1 from "../screens/Modal1";
+import Modal2 from "../screens/Modal2";
 
 const AnimatedModalsStack = () => {
   const Stack = createStackNavigator();
@@ -12,6 +14,7 @@ const AnimatedModalsStack = () => {
         name="AnimatedModalsScreen"
         component={AnimatedModalsScreen}
       />
+      <Stack.Screen name="Modal1" />
     </Stack.Navigator>
   );
 };
