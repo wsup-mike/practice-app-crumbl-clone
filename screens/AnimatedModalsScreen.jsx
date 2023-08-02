dfe79a449e0f9e1f01a2376b5b3d12908836f916
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-02 09:40:18 -0500

    visible2 under Button visible

--- screens/AnimatedModalsScreen.jsx
+++ screens/AnimatedModalsScreen.jsx
@@ -50,7 +50,7 @@ const AnimatedModalsScreen = () => {
           <Button title='Show Modal2' onPress={handleShowModal2}/>
         </View>
         <Modal1 visible={visible} onClose={handleCloseModal} />
-        <Modal2 visible={visible} onClose={handleCloseModal2} />
+        <Modal2 visible={visible2} onClose={handleCloseModal2} />
         <Modal1 
         
         />
