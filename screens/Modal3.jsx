707408d2272bf11fcfced48b1252917c63561ed6
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-03 13:39:08 -0500

    Make parent Animated.View

--- screens/Modal3.jsx
+++ screens/Modal3.jsx
@@ -1,5 +1,6 @@
-import { View, Text, SafeAreaView, Button, Animated, Dimensions } from 'react-native'
+import { View, Text, SafeAreaView, Button, Animated, Dimensions, Easing } from 'react-native'
 import React , { useRef, useEffect }from 'react'
+import { useFocusEffect } from '@react-navigation/native';
 
 const Modal3 = ({ visible3, onClose }) => {
     // Destructure hieght from screen Dimensions
@@ -14,14 +15,37 @@ const Modal3 = ({ visible3, onClose }) => {
         console.log(visible3)
     }
 
+    useEffect(() => {
+        if (visible3) {
+            Animated.timing(transY, {
+                toValue: 0,
+                duration: 225,
+                easing: Easing.inOut(Easing.ease),
+                useNativeDriver: true,
+            }).start();
+        } else {
+            Animated.timing(transY, {
+                toValue: height,
+                duration: 225,
+                easing: Easing.inOut(Easing.ease),
+                useNativeDriver: true,
+        }).start()
+        }
+    }, [visible3]);
+
 
     return (
-        <View className='absolute flex-1 justify-center h-screen w-screen bg-green-400 mt-9 rounded-2xl'>
+        <Animated.View 
+            className='absolute flex-1 justify-center h-screen w-screen bg-green-400 mt-9 rounded-2xl'
+            style={{
+                transform: [{ translateY: height}]
+            }}
+        >
             <View className='flex-1 justify-center items-center'>
                 <Text>Modal3</Text>
                 <Button title='Close Modal 3' onPress={onPress}/>
             </View> 
-        </View>
+        </Animated.View>
     )
 }
 
