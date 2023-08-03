f531e81f910baf4b77f0c7ed23d31f4109eadebc
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-03 13:18:55 -0500

    Add 'ref' object saved to transY

--- screens/Modal3.jsx
+++ screens/Modal3.jsx
@@ -1,15 +1,24 @@
-import { View, Text, SafeAreaView, Button } from 'react-native'
+import { View, Text, SafeAreaView, Button, Animated, Dimensions } from 'react-native'
 import React , { useRef, useEffect }from 'react'
 
 const Modal3 = ({ visible3 }) => {
-  return (
-    <View className='flex-1 justify-center h-2/5 w-3/5 bg-green-400'>
-        <View className='flex-1 justify-center items-center'>
-            <Text>Modal3</Text>
-            <Button title='Show Modal 3' />
-        </View> 
-    </View>
-  )
+    // Destructure hieght from screen Dimensions
+    const { height } = Dimensions.get('screen')
+
+    // Create use Animated.Value for y axis starts at distance value 'height'
+    const transY = useRef(new Animated.Value(height)).current
+
+    
+
+
+    return (
+        <View className='absolute flex-1 justify-center h-screen w-screen bg-green-400 mt-9 rounded-2xl'>
+            <View className='flex-1 justify-center items-center'>
+                <Text>Modal3</Text>
+                <Button title='Close Modal 3' />
+            </View> 
+        </View>
+    )
 }
 
 export default Modal3
