6d2fc42f8bb7b8a806e977150c7b8be28cf9466d
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-03 13:07:33 -0500

    Give Modal styleing

--- screens/Modal3.jsx
+++ screens/Modal3.jsx
@@ -3,7 +3,7 @@ import React , { useRef }from 'react'
 
 const Modal3 = ({ visible3 }) => {
   return (
-    <View className='flex-1 justify-center'>
+    <View className='flex-1 justify-center h-2/5 w-3/5 bg-green-400'>
         <View className='flex-1 justify-center items-center'>
             <Text>Modal3</Text>
             <Button title='Show Modal 3' />
