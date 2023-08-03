e5ac20135e258067ae9ee7911f3f7b6a0ddd7a9a
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-03 13:04:00 -0500

    Add button title

--- screens/Modal3.jsx
+++ screens/Modal3.jsx
@@ -2,12 +2,12 @@ import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import { Button } from 'react-native';
 
-const Modal3 = () => {
+const Modal3 = ({ visible3 }) => {
   return (
     <View className='flex-1 justify-center'>
         <View className='flex-1 justify-center items-center'>
             <Text>Modal3</Text>
-            <Button />
+            <Button title='Show Modal 3' />
         </View> 
     </View>
   )
