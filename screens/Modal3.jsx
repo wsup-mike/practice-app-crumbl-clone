bcab8def0a674b7afc7faef95fe51dd138a7ab57
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-03 12:51:57 -0500

    Cr8 Modal3 screen

---
+++ screens/Modal3.jsx
@@ -0,0 +1,16 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+import { Button } from 'react-native';
+
+const Modal3 = () => {
+  return (
+    <View className='flex-1 justify-center'>
+        <View className='flex-1 justify-center items-center'>
+            <Text>Modal3</Text>
+            <Button />
+        </View> 
+    </View>
+  )
+}
+
+export default Modal3
